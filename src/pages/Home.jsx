f4ed8bde4ff053d1bd3a7f043e6fd7ee9183da22
svenalpha <svalphaforsale@yahoo.com>
2024-02-24 18:26:56 +0700

    message

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -6,7 +6,7 @@ import { multiplyAndAdd } from '../multiply'
 export default function Home() {
   return (
     <>
-      <h1>vr12        Home          no 11      16:23     21/02/2024 </h1>
+      <h1>vr12        Home          no 12      18:25     21/02/2024 </h1>
       <div>{addAndMultiply(1, 2, 3)}</div>   
       <div>{multiplyAndAdd(1, 2, 3)}</div>  
     </>
--- vite.config.js
+++ vite.config.js
@@ -13,8 +13,8 @@ export default defineConfig({
   
   base: '/',
 
-  server:{proxy:{"/apixx": { //target: "http://192.0.0.1:5173",  
-                             target: "https://vr12.onrender.com",
+  server:{proxy:{"/apixx": { target: "http://192.0.0.1:5173",  
+                             //target: "https://vr12.onrender.com",
                               changeOrigin: true,
                               secure: false,
                               ws: true, 
