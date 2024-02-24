291fa6728bb5f02c073dcdd9461ab614c3573deb
svenalpha <svalphaforsale@yahoo.com>
2024-02-24 18:34:55 +0700

    message

--- vite.config.js
+++ vite.config.js
@@ -13,7 +13,8 @@ export default defineConfig({
   
   base: '/',
 
-  server:{proxy:{"/apixx": { target: "http://192.0.0.1:5173",  
+  server:{port:8080,
+          proxy:{"/apixx": { target: "http://192.0.0.1:5173",  
                              //target: "https://vr12.onrender.com",
                               changeOrigin: true,
                               secure: false,
