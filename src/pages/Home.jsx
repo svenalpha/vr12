a1ba0abb7336de7aab5501220cadc2d82d05cdf6
svenalpha <svalphaforsale@yahoo.com>
2024-03-02 15:58:04 +0700

    message

---
+++ src/pages/Home.jsx
@@ -0,0 +1,25 @@
+
+import { addAndMultiply } from '../add'
+import { multiplyAndAdd } from '../multiply'
+
+
+
+export default function Home() {
+
+ 
+
+
+
+  return (
+    <>
+      <h1>vr12        Home          no 24      15:44    02/03/2024 </h1>
+      <div>{addAndMultiply(1, 2, 3)}</div>   
+      <div>{multiplyAndAdd(1, 2, 3)}</div>  
+    </>
+  )
+}
+
+
+
+
+    
---
+++ vite.config.js
@@ -0,0 +1,51 @@
+import { resolve } from "path"; 
+import path from "path";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+const outDiry = resolve(__dirname,"dist");  console.log("outDiry = ",outDiry);
+const outDirx = resolve(__dirname,"dist/client");  console.log("outDirx = ",outDirx);
+const rootx  = resolve(__dirname,"src");   console.log("rootx = ",rootx);
+const root = path.join(__dirname,"src"); console.log("root = ",root);
+const outDir = path.join(__dirname,"dist/client"); console.log("outDir = ",outDir);
+
+
+export default defineConfig({
+  
+  base: '/',
+
+
+  server:{
+          proxy:{"/apixx": { target: "http://vr12.onrender.com",
+                               //target: "http://localhost:5173",
+                               //target: "http://192.168.1.5:5173",
+                                //target: "http://127.0.0.1:8080",  
+                             //target: "https://vr12.onrender.com",
+                              changeOrigin: true,
+                              secure: false,
+                              ws: true, 
+                              rewrite: (path) => path.replace(/^\/apixx/,""),
+
+                              configure: (proxy, _options) => {
+                                proxy.on('error', (err, _req, _res) => {
+                                  console.log('proxy error', err);
+                                });
+                                proxy.on('proxyReq', (proxyReq, req, _res) => {
+                                  console.log('Sending Request to the Target:', req.method, req.url);
+                                });
+                                proxy.on('proxyRes', (proxyRes, req, _res) => {
+                                  console.log('Received Response from the Target:', proxyRes.statusCode, req.url);
+                                });
+                              },                          
+
+
+
+                            },
+                },
+         },
+  plugins: [react()],
+  build: {
+    outDir: "/dist/",
+    emptyOutDir: true,
+    minify: false,
+  },
+})
