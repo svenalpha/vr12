046d52017e8a66c577976ba0b5930f4ca171571b
svenalpha <svalphaforsale@yahoo.com>
2024-04-28 10:35:12 +0700

    message

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -13,7 +13,7 @@ export default function Home() {
   return (
     <>
        
-      <h1>vr12   .......  Home   ...........    no 45  ......    10:17  ........  28/04/2024 </h1>
+      <h1>vr12   .......  Home   ...........    no 46  ......    10:33  ........  28/04/2024 </h1>
       <div>{addAndMultiply(1, 2, 3)}</div> 
       <div>{multiplyAndAdd(1, 2, 3)}</div>  
     </>
